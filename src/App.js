8b631513e81b190d3446072f814ca4c8c7e896f4
Manuel Transfeld <auipga@gmail.com>
2019-10-27 10:26:25 +0100

    Multi-Insel, Cleanup

--- src/App.js
+++ src/App.js
@@ -15,21 +15,38 @@ class App extends Component {
   ];
   step = 50;
   precision = 2;
-  defaultState = {
-    population: {
-      level_o: {
-        0: 3000,
-        1: 2000,
-        2: 500
+  emptyIsland = {
+    oldWorld: {
+      name: 'Neue Insel (Alte Welt)',
+      population: {
+        level: [
+          0,
+          0,
+          0,
+          0,
+          0,
+        ],
       },
-      level: [
-        0,
-        0,
-        0,
-        0,
-        0,
-      ],
+      buildings: {
+        Marketplace: 1
+      }
     },
+    newWorld: {
+      name: 'Neue Insel (Neue Welt)',
+      population: {
+        level: [
+          0,
+          0,
+        ],
+      },
+      buildings: {
+      }
+    },
+  }
+  defaultState = {
+    islands: [
+      JSON.parse(JSON.stringify(this.emptyIsland.oldWorld))
+    ],
   };
   componentDidMount() {
     // window.addEventListener('wheel', (e) => this.handleWheel(e), {passive: false});
@@ -39,9 +56,9 @@ class App extends Component {
     console.log('componentWillUnmount');
     document.removeEventListener('wheel', this.handleWheel);
   }
-  handleWheel = (event, i, direction, move = false, relativeTarget = 0) => {
+  handleWheel = (event, islandKey, tierKey, direction, move = false, relativeTarget = 0) => {
     event.preventDefault()
-    this.changePopulationLevel(i, direction, move, relativeTarget)
+    this.changePopulationLevel(islandKey, tierKey, direction, move, relativeTarget)
   }
 
   needs = [
@@ -180,6 +197,7 @@ class App extends Component {
   ]
 
   constructor (props) {
+    // localStorage.clear();
     super(props);
 
     let oldState = localStorage.getItem('state');
@@ -196,66 +214,70 @@ class App extends Component {
     return need.provides.reduce((prev, next, i) => prev + population[i + need.tier - 1] / next, 0);
     // return need.consumption.reduce((prev, next, i) => prev + next * population[i + need.tier - 1], 0);
   }
-  changePopulationLevel_o = (i, number) => {
-    this.setState(prevState => ({
-      ...prevState,
-        population: {
-          ...prevState.population,
-            level: {
-              ...prevState.population.level,
-              [i]: this.state.population.level[i] + number
-            }
-        }
-    }));
-  }
-  changePopulationLevel = (i, direction, move = false, relativeTarget = 0) => {
+  changePopulationLevel = (islandKey, tierKey, direction, move = false, relativeTarget = 0) => {
     let number = direction * this.step
-    let level = this.state.population.level;
-    let NewValue = (isNaN(level[i]) ? 0 : level[i]) + number;
+    let population = this.state.islands[islandKey].population.level;
+    let NewValue = (isNaN(population[tierKey]) ? 0 : population[tierKey]) + number;
     let NewValueButMinimum0 = Math.max(NewValue, 0);
-    level[i] = NewValueButMinimum0;
+    population[tierKey] = NewValueButMinimum0;
 
     // restore missing difference
     if (move === true) {
       let diff = NewValue - NewValueButMinimum0;
-      NewValue = (isNaN(level[i+relativeTarget]) ? 0 : level[i+relativeTarget]) + diff - number;
+      NewValue = (isNaN(population[tierKey+relativeTarget]) ? 0 : population[tierKey+relativeTarget]) + diff - number;
       NewValueButMinimum0 = Math.max(NewValue, 0);
-      level[i+relativeTarget] = NewValueButMinimum0;
+      population[tierKey+relativeTarget] = NewValueButMinimum0;
     }
+
+    this.setState(prevState => ({
+          ...prevState.population,
+            level: population
+    }), () => {
+      localStorage.setItem('state', JSON.stringify(this.state));
+    });
+  }
+  setPopulationLevel = (islandKey, tierKey, number) => {
+    let population = this.state.islands[islandKey].population.level;
+    population[tierKey] = number ? parseInt(number) : 0
 
     this.setState(prevState => ({
       ...prevState,
         population: {
           ...prevState.population,
-            level: level
+            level: population
         }
     }), () => {
       localStorage.setItem('state', JSON.stringify(this.state));
     });
-
   }
-  setPopulationLevel_o = (i, number) => {
-    this.setState(prevState => ({
-      ...prevState,
-        population: {
-          ...prevState.population,
-            level: {
-              ...prevState.population.level,
-              [i]: parseInt(number)
-            }
-        }
-    }));
-  }
-  setPopulationLevel = (i, number) => {
-    let level = this.state.population.level;
-    level[i] = number ? parseInt(number) : 0
+  setIslandName = (islandKey, name) => {
+    let islands = this.state.islands
+    islands[islandKey].name = name
 
     this.setState(prevState => ({
       ...prevState,
-        population: {
-          ...prevState.population,
-            level: level
-        }
+      islands: islands
+    }), () => {
+      localStorage.setItem('state', JSON.stringify(this.state));
+    });
+  }
+  addIsland(worldKey) {
+    let islands = this.state.islands
+    islands.push(JSON.parse(JSON.stringify(this.emptyIsland[worldKey])))
+
+    this.setState(prevState => ({
+      ...prevState,
+      islands: islands
+    }), () => {
+      localStorage.setItem('state', JSON.stringify(this.state));
+    });
+  }
+  deleteIsland(islandKey) {
+    let islands = this.state.islands.slice(0, islandKey).concat(this.state.islands.slice(islandKey+1))
+
+    this.setState(prevState => ({
+      ...prevState,
+      islands: islands
     }), () => {
       localStorage.setItem('state', JSON.stringify(this.state));
     });
@@ -266,46 +288,50 @@ class App extends Component {
       <div className="App">
         <Container fluid>
           <h2 className='mb-3'>Plan1800</h2>
-          <h4>Population ({ this.state.population.level.reduce((prev, next) => prev + next, 0) }) <Button onClick={this.reset} size='sm'>Reset</Button></h4>
+          {this.state.islands.map((island, islandKey) => (
+          <div>
+          <Input value={island.name} onChange={e => this.setIslandName(islandKey, e.target.value)} />
+          <h4>Population ({ island.population.level.reduce((prev, next) => prev + next, 0) }) <Button onClick={() => this.deleteIsland(islandKey)} size='sm'>löschen</Button></h4>
           <Row className='mb-5'>
             {/*<Population levels={this.populationLevels} />*/}
               {/*<PopulationItem populationLevel={levels[i]} i={i} />*/}
-            {this.populationLevels.map((populationLevel, i) => (
-                <Col key={i} className={"align-content-center" + ((this.state.population.level[i] > 0) ? ' bg-success': ' d-none-')}>
+            {this.populationLevels.map((populationLevel, tierKey) => (
+                <Col  xs={6} sm={6} md={4} lg={3} xl={2} key={tierKey}
+                      className={"align-content-center" + ((island.population.level[tierKey] > 0) ? ' bg-success': ' d-none-')}>
                   <Row className={'align-items-end'}>
-                    {/*<Col className={'col-auto' + ((i === 0) ? ' bg-warning d-none': '')}>*/}
+                    {/*<Col className={'col-auto' + ((tierKey === 0) ? ' bg-warning d-none': '')}>*/}
                     {/*  <ButtonGroup vertical size='sm'>*/}
-                    {/*    <Button onClick={() => this.changePopulationLevel(i-1, -1) + this.changePopulationLevel(i, +1)} color='primary'>&raquo;</Button>*/}
-                    {/*    <Button onClick={() => this.changePopulationLevel(i, -1) + this.changePopulationLevel(i-1, +1)} color='warning'>&lsaquo;</Button>*/}
+                    {/*    <Button onClick={() => this.changePopulationLevel(tierKey-1, -1) + this.changePopulationLevel(tierKey, +1)} color='primary'>&raquo;</Button>*/}
+                    {/*    <Button onClick={() => this.changePopulationLevel(tierKey, -1) + this.changePopulationLevel(tierKey-1, +1)} color='warning'>&lsaquo;</Button>*/}
                     {/*  </ButtonGroup>*/}
                     {/*</Col>*/}
                     <Col className=''>
-                      <img src={"/icons/population/Workforce" + (i + 1) + ".png"} alt="" className="d-block mx-auto rounded" style={{height: 40, width: 40}}/>
-                      {/*<Col xs={12} sm={10} md={6} lg={4} key={i}>*/}
-                      {/*<PopulationItem populationLevel={this.populationLevels[i]} i={i} />*/}
+                      <img src={"./icons/population/Workforce" + (tierKey + 1) + ".png"} alt="" className="d-block mx-auto rounded" style={{height: 40, width: 40}}/>
+                      {/*<Col xs={12} sm={10} md={6} lg={4} key={tierKey}>*/}
+                      {/*<PopulationItem populationLevel={this.populationLevels[tierKey]} tierKey={tierKey} />*/}
                       <InputGroup>
                         <InputGroupAddon addonType="prepend">
-                          <Button onClick={() => this.changePopulationLevel(i, -1)} color='secondary'>-</Button>
+                          <Button onClick={() => this.changePopulationLevel(islandKey, tierKey, -1)} color='secondary'>-</Button>
                         </InputGroupAddon>
                         <Input placeholder={populationLevel} title={populationLevel}
-                               value={this.state.population.level[i]}
+                               value={island.population.level[tierKey]}
                                style={{height: 62}}
                                className={'text-center'}
                             // readOnly
-                               onChange={e => this.setPopulationLevel(i, e.target.value)}
-                               onWheel={e => this.handleWheel(e, i, -Math.sign(e.deltaY))}
+                               onChange={e => this.setPopulationLevel(islandKey, tierKey, e.target.value)}
+                               onWheel={e => this.handleWheel(e, islandKey, tierKey, -Math.sign(e.deltaY))}
                         />
                         <InputGroupAddon addonType="append">
-                          <Button onClick={() => this.changePopulationLevel(i, +1)} color='secondary'>+</Button>
+                          <Button onClick={() => this.changePopulationLevel(islandKey, tierKey, +1)} color='secondary'>+</Button>
                         </InputGroupAddon>
                       </InputGroup>
                     </Col>
-                    <Col className={'col-auto '+ ((i === 4) ? ' bg-warning d-none': '')}
-                         onWheel={e => this.handleWheel(e, i+(Math.sign(e.deltaY)>0 ? 1 : 0), -1, true, -Math.sign(e.deltaY))}
+                    <Col className={'col-auto '+ ((tierKey === 4) ? ' bg-warning d-none': '')}
+                         onWheel={e => this.handleWheel(e, islandKey, tierKey+(Math.sign(e.deltaY)>0 ? 1 : 0), -1, true, -Math.sign(e.deltaY))}
                     >
                       <ButtonGroup vertical size='sm'>
-                        <Button onClick={() => this.changePopulationLevel(i+0, -1, true, +1)} color='primary'>&raquo;</Button>
-                        <Button onClick={() => this.changePopulationLevel(i+1, -1, true, -1)} color='warning'>&lsaquo;</Button>
+                        <Button onClick={() => this.changePopulationLevel(islandKey, tierKey+0, -1, true, +1)} color='primary'>&raquo;</Button>
+                        <Button onClick={() => this.changePopulationLevel(islandKey, tierKey+1, -1, true, -1)} color='warning'>&lsaquo;</Button>
                       </ButtonGroup>
                     </Col>
                   </Row>
@@ -314,9 +340,9 @@ class App extends Component {
 
             {/*<div className="w-100 m-3"></div>*/}
             <Col className="align-content-center d-none-">
-              <img src={"/icons/population/Population.png"} alt="" className={"d-block mx-auto rounded"} style={{height: 40, width: 40}}/>
+              <img src={"./icons/population/Population.png"} alt="" className={"d-block mx-auto rounded"} style={{height: 40, width: 40}}/>
               <InputGroup>
-                <Input value={'∑ ' + this.state.population.level.reduce((prev, next) => prev + next, 0)}
+                <Input value={'∑ ' + island.population.level.reduce((prev, next) => prev + next, 0)}
                        style={{height: 62}}
                        className={'text-center'}
                        readOnly
@@ -331,11 +357,11 @@ class App extends Component {
               <Row>
                 <Col sm={12}>
                   {this.needs.filter(function (need) {
-                    let firstTierRequireCount = this.state.population.level[need.tier-1];
+                    let firstTierRequireCount = island.population.level[need.tier-1];
                     /** auf dieser insel */
-                    let oneAboveRequirementExists = 0 < this.state.population.level[need.tier];
+                    let oneAboveRequirementExists = 0 < island.population.level[need.tier];
                     /** @todo requirement auf DIESER insel, baumöglichkeit auf IRGENDEINER insel*/
-                    let anyAboveRequirementExists = 0 < this.state.population.level.slice(need.tier).reduce((prev, next) => prev + next, 0);
+                    let anyAboveRequirementExists = 0 < island.population.level.slice(need.tier).reduce((prev, next) => prev + next, 0);
 
                     return firstTierRequireCount >= need.requirement || oneAboveRequirementExists
                         || (firstTierRequireCount > 0                && anyAboveRequirementExists);
@@ -343,15 +369,15 @@ class App extends Component {
                       <div key={key} className='my-3'>
                         <Media>
                           <Media left>
-                            <Media object src={"/icons/goods/" + need.id + ".png"} alt={need.resourceName}
+                            <Media object src={"./icons/goods/" + need.id + ".png"} alt={need.resourceName}
                                    middle style={{height: 30, width: 30}} className='mr-2'/>
                           </Media>
                           <Media body className='align-self-center'>
                             <Media heading className={'d-none'}>
                               {need.resourceName}
                             </Media>
-                            Need: {Math.ceil(this.exactNeed(this.state.population.level, need))}
-                            {' '}({this.exactNeed(this.state.population.level, need).toFixed(this.precision)})
+                            Need: {Math.ceil(this.exactNeed(island.population.level, need))}
+                            {' '}({this.exactNeed(island.population.level, need).toFixed(this.precision)})
                           </Media>
                         </Media>
                       </div>
@@ -361,11 +387,17 @@ class App extends Component {
             </Col>
             <Col className='col-auto'>
               <h4>Buildings</h4>
-              {/*<Productions productions={this.state.buildings} />*/}
+              {/*<Productions productions={island.buildings} />*/}
             </Col>
           </Row>
+          </div>
+          ))}
 
           <Row>
+            <Button onClick={() => this.addIsland("oldWorld")}>Neue Insel</Button>
+            <Button onClick={() => this.addIsland("oldWorld")}>Alte Welt</Button>
+            <Button onClick={() => this.addIsland("newWorld")}>Neue Welt</Button>
+            {/*<Button onClick={() => this.addIsland("capTrelawny")}>Cap Trelawny</Button>*/}
           </Row>
         </Container>
       </div>
