9d9b6d0f41514b6d1fed82fded57e36f0136d646
Manuel Transfeld <auipga@gmail.com>
2019-11-02 12:44:44 +0100

    Static needs with checkbox (e.g. Marketplace)

--- src/components/IslandNeeds.js
+++ src/components/IslandNeeds.js
@@ -3,25 +3,38 @@ import PropTypes from 'prop-types/';
 import GoodItem from "./GoodItem";
 import BuildingInput from "./BuildingInput";
 import Chart from "./Chart";
+import producers from "../data/game/producers";
+import Input from "reactstrap/es/Input";
 
 export default class IslandNeeds extends Component {
   render() {
     const { need, island, exactNeed, balance, fnSetBuildingCount } = this.props;
 
+    let producer = producers.find(p => p.provides === need.key);
+
     return (
       <GoodItem resource={need}>
-        <span className="mr-2">{exactNeed.toFixed(2)}</span>
+        {!producer ?
+          <Input
+            type='checkbox'
+            checked={island.buildings[need.key]}
+            onChange={e => fnSetBuildingCount(island.id, need.key, e.target.checked ? 1 : 0)}
+          />
+          : <>
+            <span className="mr-2">{exactNeed.toFixed(2)}</span>{/** @todo remove? */}
 
-        <BuildingInput
-          blend={exactNeed - island.buildings[need.key]}
-          buildingCount={island.buildings[need.key]}
-          buildingKey={need.key}
-          islandId={island.id}
-          fnSetBuildingCount={fnSetBuildingCount}
-        />
+            <BuildingInput
+              blend={exactNeed - island.buildings[need.key]}
+              buildingCount={island.buildings[need.key]}
+              buildingKey={need.key}
+              islandId={island.id}
+              fnSetBuildingCount={fnSetBuildingCount}
+            />
             <span className="mr-2"><Chart balance={island.buildings[need.key] - exactNeed}/></span>{/** @todo remove? */}
             {/*<span className="mr-2"><Chart balance={balance*producer.productionTime/60}/></span>/!** @todo remove? *!/*/}
             <span className="mr-2"><Chart balance={balance} max={3}/></span>
+          </>
+        }
       </GoodItem>
     )
   }
